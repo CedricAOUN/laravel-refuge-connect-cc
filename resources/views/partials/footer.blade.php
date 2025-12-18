6f0939c308c58c48e2a18b179fbd500c660b61ba
CedricA <cedric.j.aoun@gmail.com>
2025-12-18 10:44:09 +0100

    add main layout and assets

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<?php
+$currentYear = date('Y');
+
+?>
+<footer>
+    <div class="container">
+        <p>RefugeConnect - <?php echo $currentYear; ?> </p>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <img src="/images/logo.png" alt="RefugeConnect Logo" />
+    <nav>
+        <a>Accueil</a>
+        <a>Ajouter un animal</a>
+    </nav>
+</header>
